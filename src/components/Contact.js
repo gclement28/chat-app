3d7bac2bf6d5029aad122b1f4905587c2400261b
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-10-13 22:57:05 +0200

    edited contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,28 +2,45 @@ import React from 'react';
 import './Style.css';
 
 
-const name= "Ben Georges";
-const avatar = "https://randomuser.me/api/portraits/men/66.jpg"
-const online = true;
 
-function Contact(){
+
+function Contact(props){
     
     return(
     
-    <div className="Contact">
-    <img className = "Avatar" src={avatar}></img>
-    <p>
-    <h4>{name}</h4>
-    {online?" online" : "offline"}
-    </p>
-    
+        <div className="Contact">
+    <img className="avatar" src={props.Avatar}>
+        
+    </img>
 
+    <p className ="name">
+        {props.Name}
+    </p> 
+
+        <div className="status">
+
+            
+            
+            <div className ={props.Status ? "status-online":"status-offline"}>
+                
+            
+
+            </div>
+
+        <p>{props.Status ? "online":"offline"}
+        </p>
+            
+        
+        </div>
+    
+    
     </div>
-    )
+    );
+}
     
 
         
     
-}
+
 
 export default Contact
